fec453db48c9d24de0c37f4d5281b1eb3a029fff
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:39:02 +0000

    Automatically backed up by Learn

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def sum_square_difference(limit)
+  range = (1..limit)
+  square_sum(range) - sum_square(range)
+end
+
+def sum_square(range)
+  range.map{|n| n * n}.reduce(0, :+)
+end
+
+def square_sum(range)
+  range.to_a.reduce(0, :+) ** 2
+end
